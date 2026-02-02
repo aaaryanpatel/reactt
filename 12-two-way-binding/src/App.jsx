014326a2cfefde41987c6f632b6771bed8106464
aaaryanpatel <aryanpatel131129@gmail.com>
2026-02-02 00:20:31 -0600

    Two way binding

--- 12-two-way-binding/src/App.jsx
+++ 12-two-way-binding/src/App.jsx
@@ -1,35 +1,37 @@
+import React from 'react'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
+  const [Title, setTitle] = useState('')
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    console.log("Form submitted by", Title)
+  }
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <form onSubmit={(e) => {
+        submitHandler(e)
+      }}>
+
+      <input 
+        type="text" 
+        placeholder='Enter your name'
+        value={Title}
+        onChange={(e) => {
+          setTitle (e.target.value)
+        }}
+        
+      />
+        <button>submit</button>
+      </form>
+    </div>
   )
 }
 
 export default App
+
+
+// in short we dont directly write on website we tell react to write it on the website when i write aryan it will be in setTitle and Title will be aryan and it will show me that
