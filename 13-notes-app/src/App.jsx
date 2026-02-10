efdb5a4f53dea3ac5dc74d25666e071ab7ab2ad7
aaaryanpatel <aryanpatel131129@gmail.com>
2026-02-10 12:27:06 -0600

    WIP:Basic form UI

--- 13-notes-app/src/App.jsx
+++ 13-notes-app/src/App.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 
 const App = () => {
-
   const submitHandler = (e) => {
-    e.preventDefault()
-    console.log('submitted')
-  }
+    e.preventDefault();
+    console.log("Note added");
+  };
 
   return (
-    <div className="min-h-screen bg-black text-white">
-      <form  onSubmit={(e)=> {
-        submitHandler(e)
-      }} className="flex flex-col items-start gap-4 p-10">
+    <div className="h-screen lg:flex bg-black text-white">
+      <form
+        onSubmit={submitHandler}
+        className="flex flex-col lg:w1/2 items-start gap-4 p-10"
+      >
         <div className="flex w-1/2 flex-col gap-4">
           <input
             type="text"
@@ -21,18 +21,23 @@ const App = () => {
 
           <textarea
             placeholder="Write details"
-            className="h-24 w-full rounded border-2 bg-transparent px-5 py-2 outline-none resize-none"
+            className="h-24 w-full resize-none rounded border-2 bg-transparent px-5 py-2 outline-none"
           />
 
           <button
             type="submit"
-            className=" rounded bg-white px-5 py-2 text-black"
+            className="w-full rounded bg-white px-5 py-2 font-medium text-black"
           >
             Add Note
           </button>
-       
         </div>
       </form>
+      <div className="flex  bg-red-950  gap-5 flex-wrap p-10">
+        <h1 className="">You are notes </h1>
+        <div className="h-52 w-40 bg-white rounded"></div>
+        <div className="h-52 w-40 bg-white rounded"></div>
+
+      </div>
     </div>
   );
 };
