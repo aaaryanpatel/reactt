7c6976ec1504a5a1e6fcc9615fc01962031e2157
aaaryanpatel <aryanpatel131129@gmail.com>
2026-02-13 20:13:21 -0600

    Fixed the UI

--- 13-notes-app/src/App.jsx
+++ 13-notes-app/src/App.jsx
@@ -10,33 +10,31 @@ const App = () => {
     <div className="h-screen lg:flex bg-black text-white">
       <form
         onSubmit={submitHandler}
-        className="flex flex-col lg:w1/2 items-start gap-4 p-10"
+        className="flex flex-col lg:w-1/2 items-start gap-4 p-10"
       >
-        <div className="flex w-1/2 flex-col gap-4">
-          <input
-            type="text"
-            placeholder="Enter task heading"
-            className="w-full rounded border-2 bg-transparent px-5 py-2 outline-none"
-          />
-
-          <textarea
-            placeholder="Write details"
-            className="h-24 w-full resize-none rounded border-2 bg-transparent px-5 py-2 outline-none"
-          />
-
-          <button
-            type="submit"
-            className="w-full rounded bg-white px-5 py-2 font-medium text-black"
-          >
-            Add Note
-          </button>
-        </div>
+        <h1 className="text-3xl font-bold">Add Notes</h1>
+        <input
+          type="text"
+          placeholder="Enter Notes Heading"
+          className="px-5 w-full font-medium py-2 border-2 outline-none rounded"
+        />
+        <textarea
+          type="text"
+          className="px-5 w-full font-medium h-32 py-2 flex items-start flex-row border-2 outline-none rounded"
+          placeholder="Write Details"
+        ></textarea>
+        <button className="bg-white font-medium w-full outline-none text-black px-5 py-2 rounded">
+          Add Note
+        </button>
       </form>
-      <div className="flex  bg-red-950  gap-5 flex-wrap p-10">
-        <h1 className="">You are notes </h1>
-        <div className="h-52 w-40 bg-white rounded"></div>
-        <div className="h-52 w-40 bg-white rounded"></div>
-
+      <div className="lg:w-1/2 lg:border-l-2 p-10">
+        <h1 className="text-3xl font-bold">Recent Notes</h1>
+        <div className="flex flex-wrap gap-5 mt-5 h-full overflow-auto">
+          <div className="h-52 w-40 rounded-2xl bg-white"></div>
+          <div className="h-52 w-40 rounded-2xl bg-white"></div>
+          <div className="h-52 w-40 rounded-2xl bg-white"></div>
+          
+        </div>
       </div>
     </div>
   );
