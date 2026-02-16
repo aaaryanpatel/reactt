678a45cf31a28ca66f34fb9c82a0dbee959a20aa
aaaryanpatel <aryanpatel131129@gmail.com>
2026-02-15 20:32:06 -0600

    useEffect done

--- 16-useEffectHook/src/App.jsx
+++ 16-useEffectHook/src/App.jsx
@@ -1,34 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { useEffect, useState } from 'react'
+// usecases when your state is changing useeffect is running along the way
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+const [a, setA] = useState(0)
+const [b, setB] = useState(0)
+
+
+
+  function aChanging() {
+    console.log('a value is change');
+    
+  }
+
+  function BChanging() {
+    console.log('B value is change');
+    
+  }
+
+  useEffect(function() {
+    aChanging()
+  },[a])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   
+   
+   <div>  
+    <h1>A is {a}</h1>
+    <h1>B is {b}</h1>
+
+    <button
+      onClick={() => {
+        setA(a + 1)
+      }}>change A</button>
+    <button
+      onClick={() => {
+        setB(b - 1)
+      }}>Change B</button>
+
+   </div>
   )
 }
 
