d980789cd5d11fa34df9db44f695a24014a00559
aaaryanpatel <aryanpatel131129@gmail.com>
2026-02-16 23:55:12 -0600

    added useNavigate for navigation history

--- 22-Advancing-routing/src/pages/About.jsx
+++ 22-Advancing-routing/src/pages/About.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
-
+import React from "react";
+import { useNavigate } from "react-router-dom";
 const About = () => {
+  let navigate = useNavigate();
+
   return (
     <div>
-      <h1>
-        Aboutt
-      </h1>
-    </div>
-  )
-}
+      <button
+        onClick={() => {
+          navigate("/");
+        }}
+        className="bg-emerald-400 rounded px-5 py-2 m-2 cursor-pointer active:scale-95"
+      >
+        Return to Home Page
+      </button>
 
-export default About
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+        className="bg-emerald-400 rounded px-5 py-2 m-2 cursor-pointer active:scale-95"
+      >
+        Back
+      </button>
+
+      <h1>Aboutt</h1>
+    </div>
+  );
+};
+
+export default About;
