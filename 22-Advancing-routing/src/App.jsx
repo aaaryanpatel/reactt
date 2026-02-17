57fd5a55e1258ffd32bda6688659f21b47d063f7
aaaryanpatel <aryanpatel131129@gmail.com>
2026-02-16 22:56:08 -0600

    Added womens collection

--- 22-Advancing-routing/src/App.jsx
+++ 22-Advancing-routing/src/App.jsx
@@ -7,6 +7,7 @@ import About from './pages/About'
 import { Route, Routes} from 'react-router-dom'
 import NotFound from './pages/NotFound'
 import Men from './pages/Men'
+import Women from './pages/Women'
 
 const App = () => {
   return (
@@ -17,6 +18,8 @@ const App = () => {
         <Route path='/about' element={<About />}/>
         <Route path='/product' element={<Product />}/>
         <Route path='/product/men' element={<Men />}/>
+        <Route path='/product/women' element={<Women />}/>
+
 
         <Route path='/*' element={<NotFound />}/>
         
