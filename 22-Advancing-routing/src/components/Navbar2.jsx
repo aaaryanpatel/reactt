7733865c485b0bec6a213d1aad98445f6db1ef76
aaaryanpatel <aryanpatel131129@gmail.com>
2026-02-16 23:59:37 -0600

    added navbar2

---
+++ 22-Advancing-routing/src/components/Navbar2.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useNavigate } from "react-router-dom";
+
+
+const Navbar2 = () => {
+ let navigate = useNavigate();
+  return (
+    <div className='py-2 px-5 bg-cyan-700'>
+       <button
+        onClick={() => {
+          navigate("/");
+        }}
+        className="bg-amber-500 rounded px-5 py-2 m-2 cursor-pointer active:scale-95"
+      >
+        Return to Home Page
+      </button>
+
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+        className="bg-amber-500 rounded px-5 py-2 m-2 cursor-pointer active:scale-95"
+      >
+        Back
+      </button>
+    </div>
+  )
+}
+
+export default Navbar2
